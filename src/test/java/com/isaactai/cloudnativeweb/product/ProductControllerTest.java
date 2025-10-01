41d32174e3f3bf7a2e118c707479e399b659f618
Isaac T <kevincurry438@gmail.com>
2025-10-01 11:09:01 -0400

    test(user,product): add retrieval and update positive tests with post and put
    -

--- src/test/java/com/isaactai/cloudnativeweb/product/ProductControllerTest.java
+++ src/test/java/com/isaactai/cloudnativeweb/product/ProductControllerTest.java
@@ -1,7 +1,9 @@
 package com.isaactai.cloudnativeweb.product;
 
 import com.isaactai.cloudnativeweb.support.BaseApiTest;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -13,58 +15,169 @@ public class ProductControllerTest extends BaseApiTest {
 
     String username;
     String pwd;
+    Long userId;
+    Long productId;
 
     @BeforeEach
     void setUpUser() {
+        // create user
         long ts = System.currentTimeMillis();
         username = "first" + ts + "@example.com";
         pwd = "StrongPwd$";
 
-        String userJson = """
-            {
-              "first_name": "%s",
-              "last_name": "%s",
-              "username": "%s",
-              "password": "%s"
-            }
-            """.formatted("first", "last", username, pwd);
+        String bodyJson = userJson("first", "last", username, pwd);
 
-        given()
+        Response res = given()
                 .contentType("application/json").accept("application/json")
-                .body(userJson)
+                .body(bodyJson)
                 .when()
                 .post("/v1/user")
                 .then()
                 .statusCode(201)
-                .body("username", equalTo(username));
-    }
+                .body("username", equalTo(username))
+                .extract().response();
 
-    @Test
-    void createProduct_success() {
-        String productJson = """
-            {
-              "name": "%s",
-              "description": "%s",
-              "sku": "%s",
-              "manufacturer": "Apple",
-              "quantity": 5
-            }
-            """.formatted("MacBook Pro", "16-inch, M2 Pro", "sku-" + System.currentTimeMillis());
+        userId = res.jsonPath().getLong("id");
 
-        given()
+        // create product
+        bodyJson = productJson("MacBook Pro 14", "14-inch, M3 Pro",
+                "sku", "Apple", 2);
+
+        res = given()
                 .auth().basic(username, pwd)
                 .contentType("application/json")
-                .body(productJson)
+                .body(bodyJson)
                 .when()
                 .post("/v1/product")
                 .then()
+                .log().ifValidationFails()
                 .statusCode(201)
                 .body("id", notNullValue())
-                .body("name", equalTo("MacBook Pro"))
-                .body("sku", startsWith("sku-"))
-                .body("manufacturer", equalTo("Apple"))
-                .body("quantity", equalTo(5))
-                .body("owner_user_id", notNullValue());
+                .body("owner_user_id", equalTo(userId.intValue()))
+                .extract().response();
 
+        productId = res.jsonPath().getLong("id");
+    }
+
+    @Nested
+    class CreateProduct {
+        @Test
+        void createProduct_success() {
+            String bodyJson = productJson("MacBook Pro", "16-inch, M2 Pro",
+                    "sku", "Apple", 5);
+
+            given()
+                    .auth().basic(username, pwd)
+                    .contentType("application/json")
+                    .body(bodyJson)
+                    .when()
+                    .post("/v1/product")
+                    .then()
+                    .statusCode(201)
+                    .body("id", notNullValue())
+                    .body("name", equalTo("MacBook Pro"))
+                    .body("sku", startsWith("sku-"))
+                    .body("manufacturer", equalTo("Apple"))
+                    .body("quantity", equalTo(5))
+                    .body("owner_user_id", equalTo(userId.intValue()));
+        }
+    }
+
+    @Nested
+    class RetrieveProduct {
+        @Test
+        void getProductById_success() {
+            given()
+                    .when()
+                    .accept("application/json")
+                    .get("/v1/product/{id}", productId)
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(200)
+                    .body("id", equalTo(productId.intValue()))
+                    .body("name", equalTo("MacBook Pro 14"));
+        }
+    }
+
+    @Nested
+    class UpdateProduct {
+        @Test
+        void updateProduct_fullPut_success() {
+            String prodName = "Iphone 17 pro";
+            String newProductJson = productJson(prodName, "ios 26",
+                    "sku", "Apple", 10);
+
+            given()
+                    .auth().basic(username, pwd)
+                    .contentType("application/json")
+                    .body(newProductJson)
+                    .when()
+                    .put("/v1/product/{id}", productId)
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(204);
+
+            given()
+                    .accept("application/json")
+                    .when()
+                    .get("/v1/product/{id}", productId)
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(200)
+                    .body("id", equalTo(productId.intValue()))
+                    .body("name", equalTo(prodName));
+        }
+
+        @Test
+        void updateProduct_partialPatch_quantity_only_success() {
+            int quantity = 17;
+            String patchJson = """
+                    {
+                        "quantity": %s
+                    }""".formatted(quantity);
+
+            given()
+                    .auth().basic(username, pwd)
+                    .contentType("application/json")
+                    .body(patchJson)
+                    .when()
+                    .patch("/v1/product/{id}", productId)
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(204);
+
+            given()
+                    .accept("application/json")
+                    .when()
+                    .get("/v1/product/{id}", productId)
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(200)
+                    .body("id", equalTo(productId.intValue()))
+                    .body("quantity", equalTo(quantity));
+        }
+    }
+
+    private static String userJson(String first, String last, String email, String pwd) {
+        return """
+                {
+                  "first_name": "%s",
+                  "last_name": "%s",
+                  "username": "%s",
+                  "password": "%s"
+                }
+                """.formatted(first, last, email, pwd);
+        }
+
+    private static String productJson(String name, String desc, String sku, String manufa, int quantity) {
+        return """
+                {
+                  "name": "%s",
+                  "description": "%s",
+                  "sku": "%s",
+                  "manufacturer": "%s",
+                  "quantity": %s
+                }
+                """.formatted(name, desc, sku + "-" + System.currentTimeMillis(), manufa, quantity);
     }
 }
--- src/test/java/com/isaactai/cloudnativeweb/user/UserControllerTest.java
+++ src/test/java/com/isaactai/cloudnativeweb/user/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.isaactai.cloudnativeweb.user;
 
 import com.isaactai.cloudnativeweb.support.BaseApiTest;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -18,26 +20,18 @@ import static org.hamcrest.Matchers.*;
  */
 public class UserControllerTest extends BaseApiTest {
 
-    static Stream<Arguments> validUsers() {
-        return Stream.of(
-                Arguments.of("wen", "lin", "wenlin"),
-                Arguments.of("chalot", "wu", "chalot"),
-                Arguments.of("Adom", "chin", "adomchin")
-        );
-    }
+    String username;
+    String first;
+    String last;
+    String pwd;
+    Long userId;
+    String createdAt;
 
-    @ParameterizedTest(name = "[{index}] {0} {1} -> {2}+ts@example.com")
-    @MethodSource("validUsers")
-    void createUser_success_variousInput(String first, String last, String usernamePrefix) {
-        String username = usernamePrefix + System.currentTimeMillis() + "+ts@example.com";
-        String userJson = """
-            {
-              "first_name": "%s",
-              "last_name": "%s",
-              "username": "%s",
-              "password": "%s"
-            }
-            """.formatted(first, last, username, "StrongPwd$");
+    @BeforeEach
+    void createUserFixed() {
+        username = "people" + System.currentTimeMillis() + "+ts@example.com";
+        pwd = "StrongPwd$";
+        String userJson = userJson("people", "p", username, pwd);
 
         Response res = given()
                 .contentType("application/json")
@@ -47,20 +41,111 @@ public class UserControllerTest extends BaseApiTest {
                 .then()
                 .log().ifValidationFails()
                 .statusCode(201)
-                .body("id", notNullValue())
-                .body("username", matchesRegex("^[a-zA-Z0-9+_.-]+@(.+)$"))
-                .body("first_name", equalTo(first))
-                .body("last_name", equalTo(last))
-                .body("account_created", notNullValue())
-                .body("account_updated", notNullValue())
-                .body("$", not(hasKey("password")))
-                .extract()
-                .response();
+                .extract().response();
 
-        // check create_time is equal to update_time
-        String created = res.jsonPath().getString("account_created");
-        String updated = res.jsonPath().getString("account_updated");
-        assertThat("account_created should equal account_updated on first creation",
-                updated, equalTo(created));
+        userId = res.jsonPath().getLong("id");
+        first = res.jsonPath().getString("first_name");
+        last = res.jsonPath().getString("last_name");
+        createdAt = res.jsonPath().getString("account_created");
+    }
+
+    @Nested
+    class CreateUser {
+        static Stream<Arguments> validUsers() {
+            return Stream.of(
+                    Arguments.of("wen", "lin", "wenlin"),
+                    Arguments.of("chalot", "wu", "chalot"),
+                    Arguments.of("Adom", "chin", "adomchin")
+            );
+        }
+
+        @ParameterizedTest(name = "[{index}] {0} {1} -> {2}+ts@example.com")
+        @MethodSource("validUsers")
+        void createUser_success_variousInput(String first, String last, String usernamePrefix) {
+            String username = usernamePrefix + System.currentTimeMillis() + "+ts@example.com";
+            String userJson = userJson(first, last, username, "StrongPwd$");
+
+            Response res = given()
+                    .contentType("application/json")
+                    .body(userJson)
+                    .when()
+                    .post("/v1/user")
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(201)
+                    .body("id", notNullValue())
+                    .body("username", matchesRegex("^[a-zA-Z0-9+_.-]+@(.+)$"))
+                    .body("first_name", equalTo(first))
+                    .body("last_name", equalTo(last))
+                    .body("account_created", notNullValue())
+                    .body("account_updated", notNullValue())
+                    .body("$", not(hasKey("password")))
+                    .extract()
+                    .response();
+
+            // check create_time is equal to update_time
+            String created = res.jsonPath().getString("account_created");
+            String updated = res.jsonPath().getString("account_updated");
+            assertThat("account_created should equal account_updated on first creation",
+                    updated, equalTo(created));
+        }
+    }
+
+    @Nested
+    class RetrieveUser {
+
+        @Test
+        void getUserById_success() {
+            given()
+                    .auth().preemptive().basic(username, pwd)
+                    .accept("application/json")
+                    .when()
+                    .get("/v1/user/{id}", userId)
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(200)
+                    .body("id", equalTo(userId.intValue()))
+                    .body("username", equalTo(username))
+                    .body("first_name", notNullValue())
+                    .body("last_name", notNullValue())
+                    .body("$", not(hasKey("password")))
+                    .body("account_created", notNullValue())
+                    .body("account_updated", notNullValue());
+        }
+    }
+
+    @Nested
+    class UpdateUser {
+        @Test
+        void updateUser_fullPut_success() {
+            String userJson = """
+                {
+                  "first_name": "%s",
+                  "last_name": "%s",
+                  "password": "%s"
+                }
+                """.formatted("newFirst", "newLast", "newStrongPwd$");
+
+            given()
+                    .contentType("application/json")
+                    .auth().preemptive().basic(username, pwd)
+                    .body(userJson)
+                    .when()
+                    .put("/v1/user/{id}", userId)
+                    .then()
+                    .log().ifValidationFails()
+                    .statusCode(204);
+        }
+    }
+
+    private static String userJson(String first, String last, String email, String pwd) {
+        return """
+        {
+          "first_name": "%s",
+          "last_name": "%s",
+          "username": "%s",
+          "password": "%s"
+        }
+        """.formatted(first, last, email, pwd);
     }
 }
