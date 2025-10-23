d239901276c3fb7606cac5954e0b00cfd58df365
Isaac T <kevincurry438@gmail.com>
2025-10-23 12:18:04 -0400

    feat(test): remove CI profile activation from CloudNativeWebApplicationTests
    - CloudNativeWebApplicationTests now inherits all test configuration directly from BaseApiTest.

--- src/test/java/com/isaactai/cloudnativeweb/CloudNativeWebApplicationTests.java
+++ src/test/java/com/isaactai/cloudnativeweb/CloudNativeWebApplicationTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("ci")
 class CloudNativeWebApplicationTests extends BaseApiTest {
 
     @Test
