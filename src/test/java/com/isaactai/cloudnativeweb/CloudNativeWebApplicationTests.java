e9b82ec634afa5fb97ea580d26d5abee3b0c9118
Isaac T <kevincurry438@gmail.com>
2025-10-23 12:10:17 -0400

    feat(test): activate CI profile for CloudNativeWebApplicationTests

--- src/test/java/com/isaactai/cloudnativeweb/CloudNativeWebApplicationTests.java
+++ src/test/java/com/isaactai/cloudnativeweb/CloudNativeWebApplicationTests.java
@@ -1,10 +1,13 @@
 package com.isaactai.cloudnativeweb;
 
+import com.isaactai.cloudnativeweb.support.BaseApiTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class CloudNativeWebApplicationTests {
+@ActiveProfiles("ci")
+class CloudNativeWebApplicationTests extends BaseApiTest {
 
     @Test
     void contextLoads() {
